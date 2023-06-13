53b507a12e3c754c12137f72c21e0b169f5d5ee4
salvador <slb_18@hotmail.com>
2023-06-13 02:32:30 -0600

    Video 5
    hasta video 5

--- src/Logica/POO.java
+++ src/Logica/POO.java
@@ -17,9 +17,25 @@ public class POO {
         Alumno alu1 = new Alumno();
         Alumno alu2 = new Alumno(5,"salvador", "lopez");
         
-        System.out.println(alu2.nombre);
+        System.out.println("El id del Alumno 2 es: "+alu2.getId());
+        System.out.println("El Nombre del Alumno 2 es: "+alu2.getNombre());
+        System.out.println("El Apellido del Alumno 2 es: "+alu2.getApellido());
         
+        System.out.println("---------------------------------------");
+        alu1.setId(8);
+        alu1.setNombre("Aseneth");
+        alu1.setApellido("Zaragoza");
         
+        System.out.println("El id del Alumno 1 es: "+alu1.getId());
+        System.out.println("El Nombre del Alumno 1 es: "+alu1.getNombre());
+        System.out.println("El Apellido del Alumno 1 es: "+alu1.getApellido());
+        
+        System.out.println("---------------------------------------");
+        
+        alu1.setId(1000); // cambio de valor con SET
+        System.out.println("El id del Alumno 1 es: "+alu1.getId());
+        System.out.println("El Nombre del Alumno 1 es: "+alu1.getNombre());
+        System.out.println("El Apellido del Alumno 1 es: "+alu1.getApellido());
     }
     
 }
